cb4f0031b3dbb5aaa9a22c6cffdd80ec07b40a77
Gaetan Bloch <gaetan.bloch@gmail.com>
2020-05-27 19:10:38 +0200

    Change the value of variable closed inside a closure function

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 function createGreeter(myName) {
   return function () {
     console.log(myName);
+    setTimeout(() => {
+      console.log(myName);
+    }, 1000)
+
+    // The closure closes the environment and the variable names and not the
+    // value, then when the value changed after 1s the value have changed.
+    // The values are only looked up when the function executes  not when
+    // it's declared. Only registry of the variable name is closed.
+    myName = 'Sandra';
   };
 }
 
